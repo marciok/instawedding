6d04550b8061ddd6b2f43589efa1c4e235cc5c97
Marcio Klepacz <marcioklepacz@gmail.com>
2013-05-23 16:16:54 -0300

    Added some config on unicorn

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,2 +1,10 @@
+worker_processes 4 # amount of unicorn workers to spin up
+timeout 30         # restarts workers that hang for 30 seconds
 preload_app true   # needed for newrelic_rpm to start its agent
+
+after_fork do |server, worker|
+  Sidekiq.configure_client do |config|
+    config.redis = { size: 1 }
+  end
+end
 
