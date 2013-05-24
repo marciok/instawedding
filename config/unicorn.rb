a1366084d880de02f38acd71cd318b483ed1d17c
Marcio Klepacz <marcioklepacz@gmail.com>
2013-05-24 15:04:43 -0300

    Trying to fiz newrelic monitoring adding manually the agent

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,6 +1,6 @@
 worker_processes 4 # amount of unicorn workers to spin up
 timeout 30         # restarts workers that hang for 30 seconds
-preload_app true   # needed for newrelic_rpm to start its agent
+# preload_app true   # needed for newrelic_rpm to start its agent
 
 after_fork do |server, worker|
   Sidekiq.configure_client do |config|
