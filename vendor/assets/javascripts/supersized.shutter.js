747e5f920c8bafe58b540cd640a9c26f2874ed78
Marcio Klepacz <marcioklepacz@gmail.com>
2013-05-24 13:28:25 -0300

    Some visual improvments and effects like the loader and title fade

--- vendor/assets/javascripts/supersized.shutter.js
+++ vendor/assets/javascripts/supersized.shutter.js
@@ -231,7 +231,7 @@
 	 	/* Before Slide Transition
 		----------------------------*/
 	 	beforeAnimation : function(direction){
-      
+        $('h1').stop().animate({opacity : 1});
         $('.caption-wrapper .current-caption').removeClass('current-caption');
         $('.caption-wrapper .caption').eq(vars.current_slide).addClass('current-caption');
 
@@ -256,10 +256,10 @@
 				$('.current-thumb').removeClass('current-thumb');
 				$('li', vars.thumb_list).eq(vars.current_slide).addClass('current-thumb');
         setTimeout(function() {
-          $(vars.thumb_tray).stop().animate({bottom : 0, avoidTransforms : true}, 300 );
+          $(vars.thumb_tray).stop().animate({bottom : 0, avoidTransforms : true}, 300);
         }, 6000); // 1 second
         setTimeout(function() {
-          $(vars.thumb_tray).stop().animate({bottom : -$(vars.thumb_tray).height(), avoidTransforms : true}, 300 );
+          $(vars.thumb_tray).stop().animate({bottom : -$(vars.thumb_tray).height(), avoidTransforms : true}, 300);
         }, 2000);
         
 				
@@ -297,6 +297,8 @@
 	 	/* After Slide Transition
 		----------------------------*/
 	 	afterAnimation : function(){
+        $('h1').stop().animate({opacity : 0.3});
+        $('#thumb-list .current-thumb').next().append('<img class="img-loader" src="assets/progress.gif">');
         $.getJSON('/posts/last.json',function(data){
           if (data !== undefined) {
             console.info('calling latest, data: '+ data);
@@ -309,6 +311,8 @@
 
             console.info('updated fields');
           }
+          $('#thumb-list .current-thumb').next().find('.img-loader').remove();
+          console.info($('#thumb-list .current-thumb').next().find('.img-loader'));
         });
 
 	 		if (api.options.progress_bar && !vars.is_paused) theme.progressBar();	//  Start progress bar
