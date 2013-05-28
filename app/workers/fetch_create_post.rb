e90c07dc3f5a96b4b6c8d351e01afb848eb0c9a7
Marcio Klepacz <marcioklepacz@gmail.com>
2013-05-27 22:34:21 -0300

    Added another validation on fetch_create post

--- app/workers/fetch_create_post.rb
+++ app/workers/fetch_create_post.rb
@@ -3,18 +3,14 @@ class FetchCreatePost
 
 
   def perform
-    conn = Faraday.new do |faraday|
-      faraday.adapter  Faraday.default_adapter
-    end
     insta_post = Instagram.tag_recent_media(INSTA_TAG).last
     if insta_post and insta_post.caption
-      resp = conn.get(insta_post.images.standard_resolution.url)
-      if resp.status == 200
-        post = Post.where(insta_id: insta_post.id).first_or_create!(
-          insta_id: insta_post.id,
-          author: insta_post.caption.from.username,
-          text: insta_post.caption.text
-        )
+      post = Post.where(insta_id: insta_post.id).first_or_create!(
+        insta_id: insta_post.id,
+        author: insta_post.caption.from.username,
+        text: insta_post.caption.text
+      )
+      if post
         post.image = Image.new(
           standart: insta_post.images.standard_resolution.url,
           thumbnail: insta_post.images.thumbnail.url,
