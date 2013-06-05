ae321c3138eb2e9d203cf1d10f0cf686ccb38c67
Marcio Klepacz <marcioklepacz@gmail.com>
2013-06-05 12:14:13 -0300

    Changed INSTA_TAG to a generic tag

--- config/initializers/instagram.rb
+++ config/initializers/instagram.rb
@@ -1,6 +1,6 @@
 CLI_ID = 'd4277639a4af44c0b0448a83edbf9cb2'
 SECRET = 'fc45fd720df948c5b0c963ec14a66b85'
-INSTA_TAG = 'CasamentoPaulinhaePupo'
+INSTA_TAG = 'Wedding'
 
 
 Instagram.configure do |config|
